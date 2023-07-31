d137821158a6e9edecd47f9e24aca9384fdf18e3
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-31 23:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// script.js
+
+function rotateLine() {
+    var line = document.getElementById("line");
+    var rotationDegree = 360; // Change this value to adjust the rotation angle (in degrees)
+
+    line.style.transform = `rotate(${rotationDegree}deg)`;
+}
